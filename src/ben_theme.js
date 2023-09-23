112598b58077607954d6e17e4a06dfcd8ac07a59
Ben Davidson <bdavidson@lynk.world>
2023-09-23 08:25:24 -0400

    Project setup and theme

---
+++ src/ben_theme.js
@@ -0,0 +1,101 @@
+
+export const ben_theme = {
+	name: 'my-custom-theme',
+	properties: {
+		// =~= Theme Properties =~=
+		"--theme-font-family-base": `system-ui`,
+		"--theme-font-family-heading": `system-ui`,
+		"--theme-font-color-base": "0 0 0",
+		"--theme-font-color-dark": "255 255 255",
+		"--theme-rounded-base": "0px",
+		"--theme-rounded-container": "0px",
+		"--theme-border-base": "1px",
+		// =~= Theme On-X Colors =~=
+		"--on-primary": "0 0 0",
+		"--on-secondary": "255 255 255",
+		"--on-tertiary": "0 0 0",
+		"--on-success": "0 0 0",
+		"--on-warning": "255 255 255",
+		"--on-error": "255 255 255",
+		"--on-surface": "0 0 0",
+		// =~= Theme Colors  =~=
+		// primary | #CFCFCF 
+		"--color-primary-50": "248 248 248", // #f8f8f8
+		"--color-primary-100": "245 245 245", // #f5f5f5
+		"--color-primary-200": "243 243 243", // #f3f3f3
+		"--color-primary-300": "236 236 236", // #ececec
+		"--color-primary-400": "221 221 221", // #dddddd
+		"--color-primary-500": "207 207 207", // #CFCFCF
+		"--color-primary-600": "186 186 186", // #bababa
+		"--color-primary-700": "155 155 155", // #9b9b9b
+		"--color-primary-800": "124 124 124", // #7c7c7c
+		"--color-primary-900": "101 101 101", // #656565
+		// secondary | #363636 
+		"--color-secondary-50": "225 225 225", // #e1e1e1
+		"--color-secondary-100": "215 215 215", // #d7d7d7
+		"--color-secondary-200": "205 205 205", // #cdcdcd
+		"--color-secondary-300": "175 175 175", // #afafaf
+		"--color-secondary-400": "114 114 114", // #727272
+		"--color-secondary-500": "54 54 54", // #363636
+		"--color-secondary-600": "49 49 49", // #313131
+		"--color-secondary-700": "41 41 41", // #292929
+		"--color-secondary-800": "32 32 32", // #202020
+		"--color-secondary-900": "26 26 26", // #1a1a1a
+		// tertiary | #BDBDBD 
+		"--color-tertiary-50": "245 245 245", // #f5f5f5
+		"--color-tertiary-100": "242 242 242", // #f2f2f2
+		"--color-tertiary-200": "239 239 239", // #efefef
+		"--color-tertiary-300": "229 229 229", // #e5e5e5
+		"--color-tertiary-400": "209 209 209", // #d1d1d1
+		"--color-tertiary-500": "189 189 189", // #BDBDBD
+		"--color-tertiary-600": "170 170 170", // #aaaaaa
+		"--color-tertiary-700": "142 142 142", // #8e8e8e
+		"--color-tertiary-800": "113 113 113", // #717171
+		"--color-tertiary-900": "93 93 93", // #5d5d5d
+		// success | #949494 
+		"--color-success-50": "239 239 239", // #efefef
+		"--color-success-100": "234 234 234", // #eaeaea
+		"--color-success-200": "228 228 228", // #e4e4e4
+		"--color-success-300": "212 212 212", // #d4d4d4
+		"--color-success-400": "180 180 180", // #b4b4b4
+		"--color-success-500": "148 148 148", // #949494
+		"--color-success-600": "133 133 133", // #858585
+		"--color-success-700": "111 111 111", // #6f6f6f
+		"--color-success-800": "89 89 89", // #595959
+		"--color-success-900": "73 73 73", // #494949
+		// warning | #363636 
+		"--color-warning-50": "225 225 225", // #e1e1e1
+		"--color-warning-100": "215 215 215", // #d7d7d7
+		"--color-warning-200": "205 205 205", // #cdcdcd
+		"--color-warning-300": "175 175 175", // #afafaf
+		"--color-warning-400": "114 114 114", // #727272
+		"--color-warning-500": "54 54 54", // #363636
+		"--color-warning-600": "49 49 49", // #313131
+		"--color-warning-700": "41 41 41", // #292929
+		"--color-warning-800": "32 32 32", // #202020
+		"--color-warning-900": "26 26 26", // #1a1a1a
+		// error | #363636 
+		"--color-error-50": "225 225 225", // #e1e1e1
+		"--color-error-100": "215 215 215", // #d7d7d7
+		"--color-error-200": "205 205 205", // #cdcdcd
+		"--color-error-300": "175 175 175", // #afafaf
+		"--color-error-400": "114 114 114", // #727272
+		"--color-error-500": "54 54 54", // #363636
+		"--color-error-600": "49 49 49", // #313131
+		"--color-error-700": "41 41 41", // #292929
+		"--color-error-800": "32 32 32", // #202020
+		"--color-error-900": "26 26 26", // #1a1a1a
+		// surface | #FFFFFF 
+		"--color-surface-50": "255 255 255", // #ffffff
+		"--color-surface-100": "255 255 255", // #ffffff
+		"--color-surface-200": "255 255 255", // #ffffff
+		"--color-surface-300": "255 255 255", // #ffffff
+		"--color-surface-400": "255 255 255", // #ffffff
+		"--color-surface-500": "255 255 255", // #FFFFFF
+		"--color-surface-600": "230 230 230", // #e6e6e6
+		"--color-surface-700": "191 191 191", // #bfbfbf
+		"--color-surface-800": "153 153 153", // #999999
+		"--color-surface-900": "125 125 125", // #7d7d7d
+
+	}
+}
