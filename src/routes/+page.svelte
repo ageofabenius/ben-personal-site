086e871931e0cbd7b639e8a3c3baa751e885559c
Ben Davidson <bfd4542@gmail.com>
2025-12-30 16:53:07 -0500

    WIP Laptop CSS image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,94 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+Page
+<div class="flex items-center justify-center w-full h-dvh">
+    
+    <!-- Laptop -->
+    <div class="perspective-midrange">
+        
+        <!-- Laptop screen -->
+        <div class="
+          w-240 h-160
+          bg-gray-400
+          rounded-4xl
+          border-gray-600 border-2
+
+          shadow-[inset_4px_4px_0_0_rgba(255,255,255,0.22),inset_-4px_-4px_0_0_rgba(0,0,0,0.35)]
+          
+          origin-bottom
+          -rotate-x-2
+
+          p-4 pb-8
+        ">
+        
+        <!-- Screen contents -->
+        <div class="size-full grid grid-cols-5 grid-rows-5 gap-2 items-center">
+            <!-- Logo -->
+            <!-- Position in center of screen -->
+            <div class="col-start-3 row-start-3 flex items-center justify-center">
+                <!-- Style the logo div -->
+                <div class="
+                  size-24
+                  bg-gray-500/30
+                  flex items-center justify-center
+                  rounded-full
+                ">
+                    <div class="text-center font-bold text-5xl">B</div>
+                </div>
+            </div>
+            <div>A</div>
+            <div>D</div>
+            <div>E</div>
+            <div>F</div>
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+            <div>D</div>
+            <div>E</div>
+            <div>F</div>
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+            <div>D</div>
+            <div>E</div>
+            <div>F</div>
+            <div>A</div>
+            <div>B</div>
+            <div>C</div>
+            <div>D</div>
+            <div>E</div>
+            
+        </div>
+
+        </div>
+        <!-- Laptop base -->
+        <div class="
+          w-240 h-8
+          bg-gray-400
+          rounded-lg
+          border-gray-600 border-2
+
+          shadow-[inset_0_2px_0_rgba(0,0,0,0.35),inset_0_0_0_4px_rgba(255,255,255,0.08),0_18px_35px_rgba(0,0,0,0.45)]
+        ">
+            <!-- Laptop hinges -->
+            <div class="
+              w-32 h-14
+              absolute
+              left-1/5 -translate-x-1/2
+              bottom-0
+              bg-gray-400
+              border-gray-600 border-2
+              shadow-[1px_0_0_0_rgba(0,0,0,1),-1px_0_0_0_rgba(0,0,0,1),0_-1px_0_0_rgba(0,0,0,1),inset_2px_4px_0_0_rgba(255,255,255,0.22),inset_-2px_-6px_0_0_rgba(0,0,0,0.30)]
+            "/>
+            <div class="
+              w-32 h-14
+              absolute
+              right-1/5 translate-x-1/2
+              bottom-0
+              bg-gray-400
+              border-gray-600 border-2
+              shadow-[1px_0_0_0_rgba(0,0,0,1),-1px_0_0_0_rgba(0,0,0,1),0_-1px_0_0_rgba(0,0,0,1),inset_2px_4px_0_0_rgba(255,255,255,0.22),inset_-2px_-6px_0_0_rgba(0,0,0,0.30)]
+            "/>
+            
+        </div>
+    </div>
+</div>
+
