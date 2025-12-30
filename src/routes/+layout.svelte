31f0e9ed104afcf5c39d31c9dc69a5988f81f7ba
Ben Davidson <bfd4542@gmail.com>
2025-12-30 14:41:25 -0500

    Wiped and recreated empty sveltekit app

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,35 +1,9 @@
-<script>
-	import '../app.postcss';
-	import { AppShell, AppBar, AppRail, AppRailAnchor } from '@skeletonlabs/skeleton';
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
 </script>
 
-<!-- App Shell -->
-<AppShell>
-	<svelte:fragment slot="header">
-		<!-- App Bar -->
-		<AppBar>
-			<svelte:fragment slot="lead">
-				<strong class="text-xl uppercase">
-					<a href="/" class="btn hover:variant-filled-primary">Ben Davidson</a>
-				</strong>
-			</svelte:fragment>
-			<svelte:fragment slot="default">Test site, please ignore</svelte:fragment>
-			<svelte:fragment slot="headline">Projects</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<div>
-					<a href="/" class="btn hover:variant-filled-primary">Home</a>
-					<a href="/about" class="btn hover:variant-filled-primary">About</a>
-				</div>
-			</svelte:fragment>
-		</AppBar>
-	</svelte:fragment>
-	<svelte:fragment slot="sidebarLeft">
-		<AppRail>
-			<svelte:fragment slot="lead">
-				<AppRailAnchor href="/projects/personal-site">This site</AppRailAnchor>
-			</svelte:fragment>
-		</AppRail>
-	</svelte:fragment>
-	<!-- Page Route Content -->
-	<slot />
-</AppShell>
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+{@render children()}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,2 @@
-<script>
-	import { Avatar, filter, NoirLight } from '@skeletonlabs/skeleton';
-	import MyPhoto from '$lib/assets/my_photo.jpeg';
-	import { fade } from 'svelte/transition';
-</script>
-
-<div class="container h-full mx-auto flex justify-center items-center flex-col space-y-4">
-	<div use:filter={'#NoirLight'}>
-		<NoirLight />
-		<Avatar src={MyPhoto} width="w-32" rounded="rounded-full" />
-	</div>
-	<div in:fade={{ delay: 1000, duration: 1000}}>
-		<!-- <hr class="w-96 h-1 bg-neutral-500 border-0" /> -->
-		<hr class="w-96 h-0.5 bg-gradient-to-r from-transparent via-neutral-500 opacity-50" />
-	</div>
-	<div in:fade={{ delay: 1000, duration: 1000}}>
-		<p class="text-center blockquote">
-			With this personal website, I look like I know what I'm doing!
-		</p>
-	</div>
-
-</div>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
