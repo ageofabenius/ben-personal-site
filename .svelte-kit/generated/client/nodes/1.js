b11bf59f52698f0bd4a713c0654cd23740d63d26
Ben Davidson <bfd4542@gmail.com>
2026-01-04 10:04:03 -0500

    Removed accidentally checked-in .svelte-kit/ build files

--- .svelte-kit/generated/client/nodes/1.js
+++
@@ -1,1 +0,0 @@
-export { default as component } from "../../../../node_modules/.pnpm/@sveltejs+kit@2.49.2_@sveltejs+vite-plugin-svelte@6.2.1_svelte@5.46.1_vite@7.3.0_@types_376c444e44e8bbde7888beea3e30c925/node_modules/@sveltejs/kit/src/runtime/components/svelte-5/error.svelte";
