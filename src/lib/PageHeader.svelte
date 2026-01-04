f261cee218cdfe26f7f661d79943b2842fa943e2
Ben Davidson <bfd4542@gmail.com>
2026-01-04 15:18:18 -0500

    Added page header fade transition

--- src/lib/PageHeader.svelte
+++ src/lib/PageHeader.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { fade } from 'svelte/transition';
 	import { LAYOUT_DEBUG } from './debug';
 
 	let { children } = $props();
@@ -14,6 +15,7 @@
 		px-[5%]
 		text-[clamp(0.5rem,5vw,2rem)] text-black/50
 		{LAYOUT_DEBUG && 'bg-blue-100'}"
+	in:fade={{ delay: 1000, duration: 300 }}
 >
 	{@render children()}
 </div>
