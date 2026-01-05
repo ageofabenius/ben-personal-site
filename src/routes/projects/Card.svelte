6053aa69704cc7c789c807b2492174a5617b3961
Ben Davidson <bfd4542@gmail.com>
2026-01-05 09:32:14 -0500

    Softened and cleaned up project card styling

--- src/routes/projects/Card.svelte
+++ src/routes/projects/Card.svelte
@@ -20,8 +20,7 @@
     grid grid-cols-1
     overflow-hidden
     rounded-lg
-    border
-    border-black/50 lg:grid-cols-3
+	lg:grid-cols-3
     {LAYOUT_DEBUG && 'bg-orange-200'}"
 >
 	<!-- Screenshot -->
@@ -35,22 +34,43 @@
 		{/if}
 	</div>
 	<!-- Text -->
-	<div class="flex flex-col gap-4 p-4 lg:col-span-2 {LAYOUT_DEBUG && 'bg-fuchsia-100'}">
-		<!-- Title -->
-		<h1 class="text-xl font-semibold {LAYOUT_DEBUG && 'bg-lime-100'}">{title}</h1>
-		<!-- Description -->
-		<p class=" {LAYOUT_DEBUG && 'bg-lime-100'}">
-			{description}
-		</p>
+	<div
+		class="
+		-mt-2
+		rounded-lg
+		border-2
+		border-black/25
+		pt-2
+		lg:col-span-2
+		lg:mt-0
+		lg:-ml-2
+		lg:pt-0
+		lg:pl-2
+		"
+	>
+		<div
+			class="
+			flex flex-col
+			gap-4
+			p-4
+			{LAYOUT_DEBUG && 'bg-fuchsia-100'}"
+		>
+			<!-- Title -->
+			<h1 class="text-xl font-semibold {LAYOUT_DEBUG && 'bg-lime-100'}">{title}</h1>
+			<!-- Description -->
+			<p class=" {LAYOUT_DEBUG && 'bg-lime-100'}">
+				{description}
+			</p>
 
-		<!-- Spacer -->
-		<div class="grow {LAYOUT_DEBUG && 'bg-neutral-300'}"></div>
+			<!-- Spacer -->
+			<div class="grow {LAYOUT_DEBUG && 'bg-neutral-300'}"></div>
 
-		<!-- Links -->
-		{#if links}
-			<div>
-				{@render links()}
-			</div>
-		{/if}
+			<!-- Links -->
+			{#if links}
+				<div>
+					{@render links()}
+				</div>
+			{/if}
+		</div>
 	</div>
 </div>
